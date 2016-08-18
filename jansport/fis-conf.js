4c1b2346b80bfcf9f0af802d3451ca7ff6efd45f
yuanshengchao <1217398472@qq.com>
2016-08-18 13:56:09 +0800

    使用md5进行加密来清除缓存，加密文件在md5中，首页源文件为default.html

---
+++ jansport/fis-conf.js
@@ -0,0 +1,35 @@
+// fis.match('*', {
+//   useHash: false
+// });
+
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+  spriter: fis.plugin('csssprites')
+})
+
+// 对 CSS 进行图片合并
+fis.match('*.css', {
+  // 给匹配到的文件分配属性 `useSprite`
+  useSprite: true
+});
+
+// 压缩资源
+fis.match('*.js', {
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  useSprite: true,
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  optimizer: fis.plugin('png-compressor')
+});
+
+
+// 设置打包的目录或者服务器，对应的地址即可
+fis.match('*.{png,jpg,gif,js,css}',{
+	release:'/md5/$0', //对应目录
+	useHash: true  //md5加密
+});
