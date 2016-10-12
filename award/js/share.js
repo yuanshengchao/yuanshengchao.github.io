9627e9347a5f81d3f2b02ffbc0f14c99bfc2c30a
yuanshengchao <1217398472@qq.com>
2016-10-12 15:50:27 +0800

    增加音乐

--- award/js/share.js
+++ award/js/share.js
@@ -1,30 +1,42 @@
 $(document).ready(function() {
-    //判断微信
-    function is_weixn() {
-            var ua = navigator.userAgent.toLowerCase();
-            if (ua.match(/MicroMessenger/i) == "micromessenger") {
-                return true;
+    $(".music").click(function() {
+        $(this).hide();
+        $(this).siblings().show();
+        var audio = document.getElementById('music1');
+        if (audio !== null) {
+            if (audio.paused) {
+                audio.play(); 
             } else {
-                return false;
+                audio.pause(); 
             }
         }
-        // 微信点击蒙版
-    $("a.wechat").click(function() {
-        if(is_weixn()){
-            var screenheight = $(window).height();
-            var bodyheight = $("body").height();
-            var lastheight = screenheight > bodyheight ? screenheight : bodyheight;
-            $("div.shadowbox").height(lastheight);
-            $("body").scrollTop(0);
-            $("body,html").css("overflow","hidden");
-            $('body').bind("touchmove",function(e){ e.preventDefault(); });
-            $("div.shadowbox").show();
-            return false;
-        }
     });
-    $("div.shadowbox").click(function() {
-        $(this).hide();
-        $("body,html").css("overflow","auto");
-         $("body").unbind("touchmove");
-    });
+    //判断微信
+    // function is_weixn() {
+    //         var ua = navigator.userAgent.toLowerCase();
+    //         if (ua.match(/MicroMessenger/i) == "micromessenger") {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    //     // 微信点击蒙版
+    // $("a.wechat").click(function() {
+    //     if(is_weixn()){
+    //         var screenheight = $(window).height();
+    //         var bodyheight = $("body").height();
+    //         var lastheight = screenheight > bodyheight ? screenheight : bodyheight;
+    //         $("div.shadowbox").height(lastheight);
+    //         $("body").scrollTop(0);
+    //         $("body,html").css("overflow","hidden");
+    //         $('body').bind("touchmove",function(e){ e.preventDefault(); });
+    //         $("div.shadowbox").show();
+    //         return false;
+    //     }
+    // });
+    // $("div.shadowbox").click(function() {
+    //     $(this).hide();
+    //     $("body,html").css("overflow","auto");
+    //      $("body").unbind("touchmove");
+    // });
 });
